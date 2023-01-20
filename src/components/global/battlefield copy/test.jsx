351783a93cbaa1426db0fe14eae274355b101008
Nxmek <valentin.laveau@gmail.com>
2023-01-20 17:47:58 +0100

    final one

--- src/components/global/battlefield copy/test.jsx
+++ src/components/global/battlefield copy/test.jsx
@@ -10,25 +10,132 @@ import { randomNumber } from "../../../utils/random";
 const Battlefield2 = () => {
   const [playerChoice, setPlayerChoice] = useState(null);
   const [iaChoice, setIaChoice] = useState(null);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [iaScore, setIaScore] = useState(0);
+  const [result, setResult] = useState("");
 
-  const playerMakeChoice = (playerChoice) => {
-    setPlayerChoice(playerChoice);
+  const playerMakeChoice = (pc) => {
+    setPlayerChoice(pc);
   };
+
   const iaMakeChoice = () => {
-    const IaRandom = randomNumber(1, 3);
-    console.log(IaRandom);
+    const iaRandom = randomNumber(1, 3);
+    switch (iaRandom) {
+      case 1:
+        setIaChoice("Rock");
+        break;
+      case 2:
+        setIaChoice("Paper");
+        break;
+      case 3:
+        setIaChoice("Scissors");
+        break;
+      default:
+        break;
+    }
   };
 
-  iaMakeChoice();
+  const ViewPlayerChoice = () => {
+    switch (playerChoice) {
+      case "Rock":
+        return Rock;
+      case "Paper":
+        return Paper;
+      case "Scissors":
+        return Scissors;
+      default:
+        break;
+    }
+  };
+  const ViewIaChoice = () => {
+    switch (iaChoice) {
+      case "Rock":
+        return Rock;
+      case "Paper":
+        return Paper;
+      case "Scissors":
+        return Scissors;
+      default:
+        break;
+    }
+  };
 
+  const reset = () => {
+    setIaChoice(null);
+    setPlayerChoice(null);
+    setResult("");
+  };
+  const fight = () => {
+    if (iaChoice !== null) {
+      if (iaChoice === playerChoice) {
+        setResult("egalite");
+      } else if (playerChoice === "Rock" && iaChoice === "Paper") {
+        setResult("L'ordi gagne");
+        addIS();
+      } else if (playerChoice === "Rock" && iaChoice === "Scissors") {
+        setResult("Tu gagnes");
+        addPS();
+      } else if (playerChoice === "Paper" && iaChoice === "Rock") {
+        setResult("Tu gagnes");
+        addPS();
+      } else if (playerChoice === "Paper" && iaChoice === "Scissors") {
+        setResult("L'ordi gagne");
+        addIS();
+      } else if (playerChoice === "Scissors" && iaChoice === "Rock") {
+        setResult("L'ordi gagne");
+        addIS();
+      } else if (playerChoice === "Scissors" && iaChoice === "Paper") {
+        setResult("Tu gagnes");
+        addPS();
+      }
+    }
+    setTimeout(() => {
+      reset();
+    }, 800);
+  };
+  const addPS = () => {
+    setPlayerScore(playerScore + 1);
+  };
+  const addIS = () => {
+    setIaScore(iaScore + 1);
+  };
+
+  useEffect(() => {
+    console.log(Math.floor());
+    console.log(`playerChoice :  ${playerChoice}`);
+    if (playerChoice !== null) {
+      iaMakeChoice();
+    }
+  }, [playerChoice]);
+
+  useEffect(() => {
+    console.log(`ia :  ${iaChoice}`);
+    ViewIaChoice();
+    fight();
+  }, [iaChoice]);
   return (
     <div className={mc.container}>
+      {!!result ? (
+        <div className={mc.result}>
+          <h3>{result}</h3>
+        </div>
+      ) : (
+        <div className={mc.score}>
+          <div>
+            <h3>{`Ton score: ${playerScore}`}</h3>
+          </div>
+          <div>
+            <h3>{`Score de l'ordi: ${iaScore}`}</h3>
+          </div>
+        </div>
+      )}
+
       <div className={mc.fightingZone}>
         <div>
           <h2>Toi</h2>
           <div className={`adaptive-img-contain ${mc.signZone}`}>
             <span>
-              <img src={Rock} alt="" />
+              <img src={ViewPlayerChoice()} alt="" />
             </span>
           </div>
         </div>
@@ -36,13 +143,13 @@ const Battlefield2 = () => {
           <h2>Ordi</h2>
           <div className={`adaptive-img-contain ${mc.signZone}`}>
             <span>
-              <img src={rockIa} alt="" />
+              <img src={ViewIaChoice()} alt="" />
             </span>
           </div>
         </div>
       </div>
       <div className={mc.rules}>
-        <p>Fait ton choix </p>
+        <h3>Fait ton choix </h3>
       </div>
       <div className={mc.choosingZone}>
         <div
--- src/components/global/navBar/NavBar.jsx
+++ src/components/global/navBar/NavBar.jsx
@@ -4,14 +4,8 @@ import mc from "./nav-bar.module.scss";
 const NavBar = () => {
   return (
     <div className={mc.container}>
-      <div className={mc.playerScore}>
-        <p>Ton score:</p>
-      </div>
       <div className={mc.title}>
         <h2>Chi-Fu-Mi</h2>
-      </div>
-      <div className={mc.iaScore}>
-        <p>Score ordi : </p>
       </div>
     </div>
   );
